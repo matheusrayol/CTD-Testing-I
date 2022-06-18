0961301b923d87661f89d390ef3f63c4c44a177b
Matheus Ferreira <matheusrbferreira@gmail.com>
2022-06-18 12:55:09 -0300

    Inclusão de exercícios de Testing I
    Inclusão de exercícios de Testing I em seu repositório exclusivo

---
+++ Aula-15/Mesa/__tests__/Util.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const Util = require('../util.js');
+
+describe('Util', () => {
+  it('has a test', () => {
+    assert(false, 'Util should have a test');
+  });
+});
